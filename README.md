dc35433b677ac7f9c45a0dfe499c02c5e47d9c00
unlucku_ <unlucku_@users.noreply.github.com>
2021-04-08 11:55:46 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Timed List
+This class is designed to be a wrapper for arraylists that alows for elements to have an expiry time. After the expiry time the elements will no longer exist through the contains method.
+
+## Todo
+
+* Rather than using it as a wrapper, make it so that it actually implements the Collection interface
+* Add thread safe version
---
+++ src/TimedList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class TimedList<T> {
+
+	private ArrayList<T> innerList;
+	private ArrayList<Long> time;
+	private long expiryTime;
+
+	public TimedList(long expiryTime) {
+		this.innerList = new ArrayList<T>();
+		this.time = new ArrayList<Long>();
+		this.expiryTime = expiryTime;
+	}
+
+	public void add(T value) {
+		innerList.add(value);
+		time.add(System.currentTimeMillis());
+	}
+
+	public void clear() {
+		innerList.clear();
+		time.clear();
+	}
+
+	public boolean contains(String e) {
+		for (int i = innerList.size()-1; i >= 0; i--) {
+			if (System.currentTimeMillis()-time.get(i) < expiryTime) {
+				if (innerList.get(i).equals(e)) {
+					return true;
+				}
+			}
+			else {
+				clear();
+				return false;
+			}
+		}
+		return false;
+	}
+}
